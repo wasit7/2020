fe1e876f801b723586699d054d3f1a70b4fa8f76
Wasit Limprasert <wasit7@gmail.com>
2020-02-13 14:41:46 +0700

    after week04

--- dsi202/week04/myproject/myapp/models.py
+++ dsi202/week04/myproject/myapp/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 
 class Property(models.Model):
+    name=models.CharField(max_length=100)
     ac=models.BooleanField()
     size=models.DecimalField(max_digits=5, decimal_places=2)
     price_per_day=models.DecimalField(max_digits=5, decimal_places=2)
+    def __str__(self):
+        return ("%s %s %s sqm."%(self.id, self.name, self.size))
 
 class Customer(models.Model):
     name=models.CharField(max_length=100)
     mobile=models.CharField(max_length=20)
     dob=models.DateField()
     gender=models.BooleanField()
+    def __str__(self):
+        return (self.name)
 
 class Rent(models.Model):
     start=models.DateTimeField()
     stop=models.DateTimeField()
     cost=models.DecimalField(max_digits=10,decimal_places=2)
     property=models.ForeignKey(Property, on_delete=models.CASCADE)
-    property=models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer=models.ForeignKey(Customer, on_delete=models.CASCADE)
