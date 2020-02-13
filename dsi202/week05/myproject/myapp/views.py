fdcdb4feb4efe06999ab5547d4bc12f2d8e2776c
Wasit Limprasert <wasit7@gmail.com>
2020-02-13 16:37:19 +0700

    after class week05

--- dsi202/week05/myproject/myapp/views.py
+++ dsi202/week05/myproject/myapp/views.py
@@ -5,3 +5,20 @@ def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
+
+def home(request):
+    html='''
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <title>Page Title</title>
+        </head>
+        <body>
+
+        <h1>This is a Heading</h1>
+        <p>This is a paragraph.</p>
+
+        </body>
+        </html>
+    '''
+    return HttpResponse(html)
--- dsi202/week05/myproject/myproject/urls.py
+++ dsi202/week05/myproject/myproject/urls.py
@@ -20,4 +20,6 @@ from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('current_datetime/', views.current_datetime, name='current_datetime'),
+    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
 ]
