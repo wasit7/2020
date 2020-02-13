22ec78c01db76c9685e5b1ed23fe25ccf2252275
Wasit Limprasert <wasit7@gmail.com>
2020-02-13 14:58:25 +0700

    after week 04 update

--- dsi202/week04/myproject/myapp/admin.py
+++ dsi202/week04/myproject/myapp/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Property, Rent, Customer
-# Register your models here.
-admin.site.register(Property)
-admin.site.register(Rent)
-admin.site.register(Customer)
+
+class RentAdmin(admin.ModelAdmin):
+    #list_display=[f.name for f in Rent._meta.fields]
+    list_display=['start','stop','cost','property','customer']
+admin.site.register(Rent,RentAdmin)
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display=[f.name for f in Customer._meta.fields]
+    list_filter=['gender']
+admin.site.register(Customer,CustomerAdmin)
+
+class PropertyAdmin(admin.ModelAdmin):
+    list_display=[f.name for f in Property._meta.fields]
+    list_editable=['price_per_day']
+admin.site.register(Property,PropertyAdmin)
+
+# admin.site.register(Property)
+# admin.site.register(Rent)
+# admin.site.register(Customer)
--- dsi202/week04/myproject/myapp/models.py
+++ dsi202/week04/myproject/myapp/models.py
@@ -22,3 +22,5 @@ class Rent(models.Model):
     cost=models.DecimalField(max_digits=10,decimal_places=2)
     property=models.ForeignKey(Property, on_delete=models.CASCADE)
     customer=models.ForeignKey(Customer, on_delete=models.CASCADE)
+    def __str__(self):
+        return ("%s %s"%(self.property, self.start))
